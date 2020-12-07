d2a27775d4b86126ad5c1b081a163b9b2ab342cd
mspcity <codepark2019@gmail.com>
2020-12-07 02:51:50 -0500

    installed axios and react-chartjs-2

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,6 +1,15 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Contents = () => {
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+        const res = await axios.get("https://api.covid19api.com/total/dayone/country/US")
+        console.log(res)
+    }
+    fetchEvents()
+  })
   return (
     <section>
        <h2>COVID-19 Cases</h2>
