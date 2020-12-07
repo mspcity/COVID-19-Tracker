1f5063e327c962ccc742bf2b797cfe992c46d3a2
mspcity <codepark2019@gmail.com>
2020-12-07 03:06:47 -0500

    sample chart tested

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,7 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { Bar, Doughnut, Line } from 'react-chartjs-2';
 import axios from 'axios'
 
 const Contents = () => {
+
+  const [confirmedData, setConfirmedData] = useState({
+    labels: ['Jan', 'Feb', 'Mar'],
+    datasets: [
+      { 
+        label: "국내 누적 확진자",
+        backgroundColor: "salmon",
+        fill: true,
+        data: [10, 5, 3]
+      }
+    ]
+  })
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -14,7 +27,12 @@ const Contents = () => {
     <section>
        <h2>COVID-19 Cases</h2>
        <div className="contents">
-
+          <div>
+            <Bar data={confirmedData} options={
+              { title: { display: true, text: "누적 확진자 추이", fontSize: 16}},
+              { legend: { display: true, position: "bottom"}}
+            } />
+          </div>
        </div>
      </section>
   )
