45769110870147e89b44d5501682f46755921d39
mspcity <codepark2019@gmail.com>
2020-12-07 00:31:12 -0500

    added Header and Contents components

---
+++ src/components/Contents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contents = () => {
+  return (
+    <section>
+       <h2>COVID-19 Cases</h2>
+       <div className="contents">
+
+       </div>
+     </section>
+  )
+}
+
+export default Contents
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+       <h1>COVID-19</h1>
+       <select className="header">
+          <option>United States</option>
+          <option>World</option>
+       </select>
+     </header>
+  )
+}
+
+export default Header
