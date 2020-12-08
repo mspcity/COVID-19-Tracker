47bcddb8b016aeca86ede09be3ca4cc0f4fca30a
mspcity <codepark2019@gmail.com>
2020-12-08 04:58:35 -0500

    added new label

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,7 @@ const Header = () => {
   return (
     <header className="header">
        <h1>COVID-19</h1>
-       <select >
-          <option>United States</option>
-          <option>World</option>
-       </select>
+          United States
      </header>
   )
 }
