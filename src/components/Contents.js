cf779b3ab386dcbb6b4acb9a31025691a483318f
mspcity <codepark2019@gmail.com>
2020-12-08 01:18:48 -0500

    added Cases Data

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -4,22 +4,55 @@ import axios from 'axios'
 
 const Contents = () => {
 
-  const [confirmedData, setConfirmedData] = useState({
-    labels: ['Jan', 'Feb', 'Mar'],
-    datasets: [
-      { 
-        label: "국내 누적 확진자",
-        backgroundColor: "salmon",
-        fill: true,
-        data: [10, 5, 3]
-      }
-    ]
-  })
+  const [confirmedData, setConfirmedData] = useState({})
+  const [quarantinedData, setQuarantinedData] = useState({})
 
   useEffect(() => {
     const fetchEvents = async () => {
         const res = await axios.get("https://api.covid19api.com/total/dayone/country/US")
-        console.log(res)
+        makeData(res.data)
+    }
+    const makeData = (items) => {
+      const arr = items.reduce((acc, cur) => {
+        const currentDate = new Date(cur.Date);
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const date = currentDate.getDate();
+        const confirmed = cur.Confirmed;
+        const active = cur.Active;
+        const death = cur.Deaths;
+        const recovered = cur.Recovered;
+
+        const findItem = acc.find(a => a.year === year && a.month === month)
+        
+        if(!findItem) {
+          acc.push({year, month, date, confirmed, active, death, recovered})
+        }
+        if(findItem && findItem.date < date) {
+          findItem.active = active;
+          findItem.death = death;
+          findItem.date = date;
+          findItem.year = year;
+          findItem.month = month;
+          findItem.recovered = recovered;
+          findItem.confirmed = confirmed;
+        }
+        return acc;
+      }, [])
+      
+
+      const labels = arr.map(a => `${a.month+1}`)
+      setConfirmedData({
+        labels,
+        datasets: [
+          { 
+            label: "Cases",
+            backgroundColor: "salmon",
+            fill: true,
+            data: arr.map(a => a.confirmed)
+          }
+        ]
+      })
     }
     fetchEvents()
   })
@@ -30,6 +63,12 @@ const Contents = () => {
           <div>
             <Bar data={confirmedData} options={
               { title: { display: true, text: "누적 확진자 추이", fontSize: 16}},
+              { legend: { display: true, position: "bottom"}}
+            } />
+          </div>
+          <div>
+            <Line data={quarantinedData} options={
+              { title: { display: true, text: "", fontSize: 16}},
               { legend: { display: true, position: "bottom"}}
             } />
           </div>
